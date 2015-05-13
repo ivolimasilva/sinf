dfd4aa5237c7dbd3ecb8c3601dcf6d8f4c2c5481
Ivo Lima da Silva <ivo.lima.da.silva@live.com.pt>
2015-05-13 23:24:57 +0100

    SQL Script updated.

--- database.sql
+++ database.sql
@@ -1,10 +1,19 @@
+DROP TABLE IF EXISTS projeto.players CASCADE;
+DROP TABLE IF EXISTS projeto.games CASCADE;
+DROP TABLE IF EXISTS projeto.questions CASCADE;
+DROP TABLE IF EXISTS projeto.gamequestions CASCADE;
+DROP TABLE IF EXISTS projeto.messages CASCADE;
+
+CREATE TYPE statusEnum AS ENUM ('online', 'busy', 'offline');
+
 CREATE TABLE projeto.players
 (
 	id BIGSERIAL PRIMARY KEY,
 	name VARCHAR (20) UNIQUE NOT NULL,
 	password VARCHAR (20) NOT NULL,
-	admin BOOLEAN,
-	online BOOLEAN
+	admin BOOLEAN NOT NULL,
+	online statusEnum NOT NULL,
+	gamesWon INTEGER NOT NULL
 );
 
 CREATE TABLE projeto.games
@@ -24,15 +33,16 @@ CREATE TABLE projeto.questions
 	wrong3 VARCHAR (30) NOT NULL
 );
 
-CREATE TABLE projeto.gameplayers
-(
-	game_id BIGINT REFERENCES projeto.games (id),
-	player_id BIGINT REFERENCES projeto.players (id)
-);
-
 CREATE TABLE projeto.gamequestions
 (
 	game_id BIGINT REFERENCES projeto.games (id),
 	question_nr INTEGER NOT NULL,
 	question_id BIGINT REFERENCES projeto.questions (id)
+);
+
+CREATE TABLE projeto.messages
+(  
+	sender_id BIGINT REFERENCES projeto.players (id) NOT NULL, 
+	receiver_id BIGINT REFERENCES projeto.players (id) NOT NULL,
+	msgText VARCHAR (30) NOT NULL
 );
